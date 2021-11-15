3c86f5a53761c5bf6568184fd086d0cc000b0333
JJ Teoh <58784686+Ripwords@users.noreply.github.com>
2021-11-15 21:25:43 +0800

    added tauri for desktop version

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import Pages from 'vite-plugin-pages'
 import PurgeIcons from 'vite-plugin-purge-icons'
 import Components from 'unplugin-vue-components/vite'
 import AutoImport from 'unplugin-auto-import/vite'
-import { VitePWA } from 'vite-plugin-pwa'
 import dayjs from 'dayjs'
 import Vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
@@ -25,24 +24,6 @@ export default defineConfig({
       ],
     }),
     PurgeIcons(),
-    VitePWA({
-      manifest: {
-        name: 'Icônes',
-        short_name: 'Icônes',
-        icons: [
-          {
-            src: '/android-chrome-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/android-chrome-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-    }),
     WindiCSS({
       safelist: 'text-2xl text-3xl text-4xl',
       preflight: {
